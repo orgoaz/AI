ed7db87a4bbf5fd1c6459e3726240f2b0eb0ed8b
Eyal Avidan <avidan.eyal@gmail.com>
2017-12-02 23:09:17 +0200

    Q25 (code only) + Q23

--- heuristics/TSPCustomHeuristic.py
+++ heuristics/TSPCustomHeuristic.py
@@ -1,14 +1,27 @@
 from heuristics import Heuristic
+from ways.tools import compute_distance
 
 # TODO : Implement as explained in the instructions
 class TSPCustomHeuristic(Heuristic):
-    _distMat = None
-    _junctionToMatIdx = None
+    _roads = None
 
     # TODO : You can add parameters if you need them
     def __init__(self, roads, initialState):
         super().__init__()
+        self._roads = roads
 
     # Estimate heuristically the minimal cost from the given state to the problem's goal
     def estimate(self, problem, state):
-        raise NotImplementedError
+        idx = state.junctionIdx
+        orders = problem.orders
+        
+        # h_e
+        h_a = max(orders.map(lambda o: self._cost(o[0], o[1])))
+        h_d = max(orders.map(lambda o: self._cost(idx, o[0])))
+        return h_a + h_d
+
+    def _cost(self, source, target:
+        coord1 = self._roads[source].coordinates
+        coord2 = self._roads[target].coordinates
+
+        return compute_distance(coord1, coord2)
